83a3b3adb25a70069cac128612f43c9350566fe8
Pluggycode <mrmagnums33@gmail.com>
2024-12-30 16:45:11 +0530

    first commit

---
+++ pod-app/components/GenerateTumbnail.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const GenerateTumbnail = () => {
+  return (
+    <div>GenerateTumbnail</div>
+  )
+}
+
+export default GenerateTumbnail
---
+++ pod-app/vite-env.d.ts
@@ -0,0 +1,7 @@
+interface importMetaEnv {
+    readonly NEXT_PUBLIC_CONVEX_URL: string;
+
+}
+interface ImportMeta {
+    readonly env: importMetaEnv;
+}
